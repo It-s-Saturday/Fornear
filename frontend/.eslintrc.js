8744f9517ab7fe2f3a323136e2d81b7a64b0e508
Jibran Absarulislam <jga26@njit.edu>
2023-06-24 01:36:18 -0400

    add extra config for eslint and prettier

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -9,9 +9,7 @@ module.exports = {
       env: {
         node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
         sourceType: 'script',
       },
@@ -22,5 +20,6 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    'import/no-extraneous-dependencies': 'off',
   },
 };
