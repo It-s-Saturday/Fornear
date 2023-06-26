832f6932dbf4212c49dd2d6b4cd741b73af9ca20
jibranabsarulislam <jga26@njit.edu>
2023-06-26 12:51:49 -0400

    spread navlinks into separate routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/navbar/Navbar';
+
 import Home from './components/Home';
 import Request from './components/Request';
+
 import Staff from './components/staff/Staff';
+import RequestList from './components/staff/RequestList';
+import Inventory from './components/staff/Inventory';
+import CreatePackage from './components/staff/CreatePackage';
+import Insert from './components/staff/Insert';
+import Update from './components/staff/Update';
+
 import Admin from './components/admin/Admin';
+
 import Background from './components/Background';
 
 function App() {
@@ -18,6 +28,12 @@ function App() {
           <Route path="home" element={<Home />} />
           <Route path="request/:_id" element={<Request />} />
           <Route path="staff" element={<Staff />} />
+          <Route path="staff/requests" element={<RequestList />} />
+          <Route path="staff/inventory" element={<Inventory />} />
+          <Route path="staff/create-package" element={<CreatePackage />} />
+          <Route path="staff/insert" element={<Insert />} />
+          <Route path="staff/update" element={<Update />} />
+
           <Route path="admin" element={<Admin />} />
         </Routes>
       </BrowserRouter>
